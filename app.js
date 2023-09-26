8f0b6d92e6e24866b4c8f348e1a50ade8f4478dd
nicolas enciso <nienciso2@gmail.com>
2023-09-26 19:41:37 -0300

    /api/sequelize

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const indexRoutes = require("./routes/indexRoutes");
 const app = express();
 const PORT = 3000;
+const productos = require("./models").productos;
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -13,7 +14,10 @@ app.use("/", indexRoutes);
 
 app.use("/admin", require("./routes/adminRoutes"));
 
-
+app.use("/api/productos", async(req,res) =>{
+    const data = await productos.findAll();
+    res.json(data);
+});
 
 app.listen(PORT, ()=>{
     console.log("server ON",PORT);
